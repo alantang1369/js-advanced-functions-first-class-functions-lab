0f1033f30c335c86628f89e1524986539b42ba8f
Alan Tang <giabao_tang@yahoo.com>
2021-07-24 14:39:38 -0700

    commit tree 709c922e0c41019df4847e340c7c7105555d36be

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(array){
+    let result = [];
+    for (let i= 0; i<2; i++){
+        result.push(array[i])
+    };
+    return result;
+}
+
+const returnLastTwoDrivers = function(array){
+    let result = [];
+    for ( let i = array.length -1 ; i> array.length -3; i--){
+        result.unshift(array[i])
+    }
+    return result;
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(num){
+    return function(fare){
+        return fare * num
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(array, f){
+    return f(array)
+}
